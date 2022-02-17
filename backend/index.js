5a3d8d4dfadbf96400d57619a0d2a614d655ad8e
Wjs0068 <87343895+Wjs0068@users.noreply.github.com>
2022-02-17 15:07:38 -0800

    fix

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,8 @@ app.use(express.json());
 
 app.use("/recipe", recipeRouters);
 
-mongoose.connect(process.env.MONGODB_CLIENT_URL);
-const port = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_CLIENT);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port: ${port}`);
 });
